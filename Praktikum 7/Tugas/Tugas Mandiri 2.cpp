60e4535e7e0758cfe8b9a0293246b43354bc9697
Firdaus Nuur Rhamadhan <93628058+Nuur-R@users.noreply.github.com>
2021-12-20 21:40:40 +0700

    Update Tugas Mandiri 2.cpp

--- Praktikum 7/Tugas/Tugas Mandiri 2.cpp
+++ Praktikum 7/Tugas/Tugas Mandiri 2.cpp
@@ -1,1 +1,34 @@
-// Belum dikerjain  :-)
+// Dik : detik 1-10 kecepatan tetap 3m/det
+//       detik 11-20 keceparan menjadi 4m/det.
+//       setiap 10 detik kecepatan bertambah 1m/det.
+
+// dit : susun program untuk mencapai jarak 100m.
+
+#include <stdio.h>
+main()
+{
+    int waktu, jarak, kecepatan, A;
+    A = 1;
+    kecepatan = 3;
+    waktu = 1;
+    jarak = 3;
+    while (jarak <= 100)
+    {
+        printf("%i = %im\n", waktu, jarak); 
+        waktu++;
+        jarak+=kecepatan;
+        while (waktu == (A*10))
+        {
+            A++;
+            kecepatan++;
+            printf("keceptan laju menjadi %im/detik\n",kecepatan);
+        }
+        if (jarak == 100)
+        {
+            printf("untuk mencapai jarak 100m kita memerlukan %i detik", waktu);
+        }
+        
+    }
+    
+    return 0;
+}
